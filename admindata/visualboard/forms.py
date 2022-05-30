efe18153f94906a2a28c8e953612713d0bca34a6
sky81219 <sky81219@github.com>
2022-05-30 15:16:03 +0900

    admin 회원가입

--- admindata/visualboard/forms.py
+++ admindata/visualboard/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
 
 from typing import Dict, Tuple
 from .models import AdminUsers
@@ -12,23 +13,18 @@ class AdminRegisterForm(UserCreationForm):
         "password:mismatch": ("비밀번호가 맞지 않습니다 다시 입력해주세요..!")
     }
     full_name = forms.CharField(
-        max_length=20, required=True, label="이름",
+        max_length=20, label="이름",
         widget=forms.TextInput(attrs={"class": "name", "placeholder": "성함"})
     )
 
     class Meta:
         model = AdminUsers
-        fields = (
-            "full_name",
-            "email",
-            "password1",
-            "password2"
-        )
+        fields = ("full_name", "email", "password1", "password2")
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         for num in range(1, 3):
-            self.fields[f"password{num}"].required = False
+            self.fields[f'password{num}'].label = f"패스워드{num}"
             self.fields[f"password{num}"].widget.attrs.update({
                 "class": f"password{num}-control",
                 "placeholder": f"password{num}-checking"
@@ -38,9 +34,9 @@ class AdminRegisterForm(UserCreationForm):
 # LoginForm
 class AdminLoginForm(AuthenticationForm):
     error_messages: Dict[str, Tuple[str]] = {
-        'invalid_login': (
+        'invalid_login': _(
             "비밀번호나 이메일이 올바르지 않습니다 다시 입력해주세요..!"),
-        'inactive': (
+        'inactive': _(
             "이 계정은 비활성화 or 인증되지 않았습니다 이메일 상태를 확인해주세요..!")
     }
 
--- admindata/visualboard/views.py
+++ admindata/visualboard/views.py
@@ -14,35 +14,25 @@ class AdminLoginView(FormView):
     form_class = AdminLoginForm
     success_url = "/admindash/base/"
 
-    def form_invalid(self, form) -> HttpResponse:
-        return super().form_invalid(form)
-    
     def form_valid(self, form) -> HttpResponse:
         email = form.cleaned_data.get("email")
         raw_password = form.cleaned_data.get("password")
-        auth_user = authenticate(self.request, email=email, password=raw_password)
+        auth_user = authenticate(self.request, username=email, password=raw_password)
         print(email, raw_password, auth_user)
         if auth_user is not None:
             login(self.request, auth_user)
         return super().form_valid(form)
 
 
-class AdminRegisterView(View):
-    form_class = AdminRegisterForm
+class AdminRegisterView(FormView):
     template_name: str = "visualboard/register.html"
-
-    def get(self, request):
-        forms = self.form_class()
-        return render(request, self.template_name, {"form": forms})
-
-    def post(self, request):
-        forms = self.form_class(request.POST)
-        msg = "올바르지 않는 형식입니다 다시입력해주세요..!"
-        if forms.is_valid():
-            forms.save()
-            return HttpResponseRedirect("/admindash/login/")
-        return render(request, self.template_name, {"form": forms, "msg": msg})
-
+    form_class = AdminRegisterForm
+    success_url = "/admindash/login/"
+    
+    def form_valid(self, form) -> HttpResponse:
+        form.save()
+        return super().form_valid(form)
+    
 
 def logout_view(request):
     logout(request)
