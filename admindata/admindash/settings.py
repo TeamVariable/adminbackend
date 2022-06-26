edb21237ac9870cfc421698d9b8317394254f2d9
sky81219 <sky81219@github.com>
2022-06-26 21:48:51 +0900

    처음으로

--- admindata/admindash/settings.py
+++ admindata/admindash/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import my_setting 
 from pathlib import Path
-from .my_setting import MY_DATABASES, MY_SECRET
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = MY_SECRET["SECRET_KEY"]
+SECRET_KEY = my_setting.MY_SECRET["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -82,7 +83,7 @@ WSGI_APPLICATION = 'admindash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = MY_DATABASES
+DATABASES = my_setting.MY_DATABASES
 
 
 # Password validation
--- admindata/my_setting.py
+++ admindata/my_setting.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
-MY_SECRET: Dict[str] = {
-    "SECRET_KEY": 'django-insecure-_1#f(vf6%obd4h!+3kzv3a6x2e+me2=e**2uvz0-!_*2w*k@p2'
+MY_SECRET: Dict = {
+    "SECRET_KEY": r'django-insecure-_1#f(vf6%obd4h!+3kzv3a6x2e+me2=e**2uvz0-!_*2w*k@p2'
 }
 
-MY_DATABASES: Dict[Dict[str]] = {
+MY_DATABASES: Dict = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'admindata',
